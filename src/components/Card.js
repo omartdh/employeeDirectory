307beba2e74dd6a7ed004537a5785a17a4aa09f0
Omar Dahmash <omartdh@omars-mbp.home>
2021-05-06 16:55:21 -0700

    modified some css

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,21 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="card text-center">
-      <div className="card-header">
+    <div className="card">
+      <div className="card-header text-center">
         <h2>{props.first} {props.last}</h2>
       </div>
       <div className="card-body">
-      <div className="text-center">
-      <img alt={props.gender} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
-      <h3>Gender: {props.gender}</h3>
-      <h3>Age: {props.age}</h3>
-      <h3>Email: {props.email}</h3>
-      <h3>Phone: {props.phone}</h3>
-      <h3>City: {props.city}</h3>
-    </div>
+        <div className="text-center" >
+          <img alt={props.gender} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} />
+           <ul class="list-group">
+              <li class="list-group-item">Gender: {props.gender}</li>
+              <li class="list-group-item">Age: {props.age}</li>
+              <li class="list-group-item">Email: {props.email}</li>
+              <li class="list-group-item">Phone: {props.phone}</li>
+              <li class="list-group-item">City: {props.city}</li>
+            </ul>
+        </div>
       </div>
     </div>
   );
